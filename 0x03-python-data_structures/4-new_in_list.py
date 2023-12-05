4a176c22fb108c3c7a3acc8608c3547146dda9f1
David ogola <Davidogola10@gmail.com>
2023-12-04 21:39:54 -0800

    Switch integer

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    # Check if idx is negative or out of range
-    if idx < 0 or idx >= len(my_list):
-        return my_list
+    cp = []
+    for i in range(len(my_list)):
+        cp.append(my_list[i])
+    if idx < 0:
+        return cp
+    elif idx >= len(my_list):
+        return cp
     else:
-        my_list[idx] = element
-        return my_list
+        cp[idx] = element
+        return cp
