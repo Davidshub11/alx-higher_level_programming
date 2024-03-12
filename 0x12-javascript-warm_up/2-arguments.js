2a49bd57fdfa9331580af2f86b58f1ce06ff2080
David ogola <Davidogola10@gmail.com>
2024-03-12 16:50:43 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,12 @@
-#!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
+#!/usr/bin/env node
+
+const args = process.argv.slice(2); // Extracting command line arguments (excluding node and script name)
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
 } else {
-  console.log('Arguments found');}
+  console.log("Arguments found");
+}
+
