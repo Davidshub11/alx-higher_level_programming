74aea262d15be482c82f422486be39def7da2c22
David ogola <Davidogola10@gmail.com>
2024-03-12 17:17:49 -0700

    Javascript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log('0');
+
+const args = process.argv.slice(2).map(Number); // Convert arguments to integers
+
+if (args.length === 0 || args.length === 1) {
+  console.log(0);
 } else {
-  const arr = process.argv.slice(2).map(Number);
-  const second = arr.sort(function (a, b) { return b - a; })[1];
-  console.log(second);
+  const sortedArgs = args.sort((a, b) => b - a); // Sort arguments in descending order
+  console.log(sortedArgs[1]);
 }
+
